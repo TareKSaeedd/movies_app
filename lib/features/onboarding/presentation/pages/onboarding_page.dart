478ca287fbbcbcc353fd5f31fbfe4069c6e4b62e
Tarek <Tareksaeed11111@gmail.com>
2025-08-19 15:24:31 +0300

    -adding initial onboarding feature

---
+++ lib/features/onboarding/presentation/pages/onboarding_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/core/constants/app_colors.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: AppColors.blackColor);
+  }
+}
