2a904692fc27d871cf1fd9956629c0c39f159b39
Tarek <Tareksaeed11111@gmail.com>
2025-08-20 22:10:06 +0300

    -remove some routes

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:movies_app/core/navigation/app_routes.dart';
 import 'package:movies_app/features/auth/presentation/pages/forget_password_page.dart';
 import 'package:movies_app/features/auth/presentation/pages/login_page.dart';
 import 'package:movies_app/features/auth/presentation/pages/register_page.dart';
-import 'package:movies_app/features/home/presentation/pages/home_page.dart';
 import 'package:movies_app/features/onboarding/presentation/pages/onboarding_page.dart';
 
 void main() {
@@ -20,11 +19,8 @@ class MyApp extends StatelessWidget {
       initialRoute: AppRoutes.onboardingRouteName,
 
       routes: {
-        AppRoutes.onboardingRouteName: (context) => OnboardingPage(),
-        AppRoutes.registerRouteName: (context) => RegisterPage(),
         AppRoutes.loginRouteName: (context) => LoginPage(),
         AppRoutes.forgetPasswordRouteName: (context) => ForgetPasswordPage(),
-        AppRoutes.homePageRouteName: (context) => HomePage(),
       },
     );
   }
