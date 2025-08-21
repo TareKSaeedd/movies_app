834dd4e1efd0737ae3116b6d10fb0fe99343c534
shady-adel-2025 <shadyadel864@gmail.com>
2025-08-21 16:24:56 +0300

    Add LoginPage UI with password visibility and toggle

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static const String loginScreenLogo = "assets/images/login_screen_logo.png";
+  static const String egyptFlag = "assets/images/egypt_flag.png";
+  static const String englishFlag = "assets/images/english_flag.png";
+  static const String gmailIcon = "assets/images/google_icon.png";
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.onboardingRouteName,
+      initialRoute: AppRoutes.loginRouteName,
 
       routes: {
         AppRoutes.onboardingRouteName: (context) => OnboardingPage(),
