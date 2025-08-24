abda8c6529f01656ac32906a853e4917949fcbe7
PolaSpary <polasapry10@gmail.com>
2025-08-25 02:23:56 +0300

    +finished home page

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.blackColor,
+      backgroundColor: AppColors.whiteColor,
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: AppColors.blackColor,
         type: BottomNavigationBarType.fixed,
