d4dd3526f47a76f19ddf91010c9844364b1290da
Tarek <Tareksaeed11111@gmail.com>
2025-09-15 09:14:11 +0300

    fix history bug : done with token of email

--- lib/core/services/history_movie_services.dart
+++ lib/core/services/history_movie_services.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
+import 'package:movies_app/core/network/auth_api/shared_pref_network.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../features/movie_details_screen/data/model/movie_details_response.dart';
 
 class HistoryMovieServices {
-  static const String key = "history_movies";
+  // static const String key = "history_movies";
 
   Future<void> addMovieToHistory(Movie movie) async {
     final prefs = await SharedPreferences.getInstance();
-    List<String> movies = prefs.getStringList(key) ?? [];
+    String? key = await SharedPrefNetwork.getCurrentUserToken();
+    List<String> movies = prefs.getStringList(key!) ?? [];
 
     String movieJson = jsonEncode(movie.toJson());
-
 
     if (!movies.contains(movieJson)) {
       movies.add(movieJson);
@@ -20,10 +21,9 @@ class HistoryMovieServices {
 
   Future<List<Movie>> getHistory() async {
     final prefs = await SharedPreferences.getInstance();
-    List<String> moviesJson = prefs.getStringList(key) ?? [];
+    String? key = await SharedPrefNetwork.getCurrentUserToken();
+    List<String> moviesJson = prefs.getStringList(key!) ?? [];
 
-    return moviesJson
-        .map((movieStr) => Movie.fromJson(jsonDecode(movieStr)))
-        .toList();
+    return moviesJson.map((movieStr) => Movie.fromJson(jsonDecode(movieStr))).toList();
   }
 }
--- lib/features/home/data/data_source/local/impl/history_movies_local_data_source_impl.dart
+++ lib/features/home/data/data_source/local/impl/history_movies_local_data_source_impl.dart
@@ -2,8 +2,8 @@ import 'package:movies_app/core/services/history_movie_services.dart';
 import 'package:movies_app/features/home/data/data_source/local/history_movies_local_data_source.dart';
 import 'package:movies_app/features/movie_details_screen/data/model/movie_details_response.dart';
 
-class HistoryMoviesLocalDataSourceImpl implements HistoryMoviesLocalDataSource{
-  HistoryMovieServices services ;
+class HistoryMoviesLocalDataSourceImpl implements HistoryMoviesLocalDataSource {
+  HistoryMovieServices services;
   HistoryMoviesLocalDataSourceImpl({required this.services});
   @override
   Future<void> addMovieToHistory(Movie movie) {
@@ -16,9 +16,5 @@ class HistoryMoviesLocalDataSourceImpl implements HistoryMoviesLocalDataSource{
     // TODO: implement getHistory
 
     return services.getHistory();
-
   }
-
-
-  
 }
